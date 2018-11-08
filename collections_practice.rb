3994058aa9aec21fbedb1c02c36d461b20749000
Thompson Plyler <nealok.g@gmail.com>
2018-11-08 04:03:35 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,16 +1,13 @@
 def begins_with_r(array)
-result = true
-split_array = []
-  array.each do |x|
-
-    
-
+  result = true
+  split_array = []
+    array.each do |x|
     split_array << x.split
-    if split_array[0] != "r"
-      result = false
-    else
-      result = true
+      if split_array[0] != "r"
+        result = false
+      else
+        result = true
+      end
     end
-  end
 result
 end
