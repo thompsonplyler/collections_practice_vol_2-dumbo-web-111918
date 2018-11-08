d58ecd69923be600735b5da65d34a5ad14c1fa41
Thompson Plyler <nealok.g@gmail.com>
2018-11-08 04:35:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,10 +1,5 @@
 require "pry"
 
-# array1 = ["ruby", "rental", "rooster"]
-# array2 = ["ruby", "ostrich", "rooster"]
-
-# array1[0].split("") #=> ["r", "u", "b", "y"]
-
 def begins_with_r(array)
 i=0
 result = false
