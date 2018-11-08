531ff4889078d7b1c15c7a54f7168b0b3291123a
Thompson Plyler <nealok.g@gmail.com>
2018-11-08 04:35:31 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,3 +20,24 @@ result = false
   end
   result
 end
+
+def contain_a
+end
+
+def first_wa
+end
+
+def remove_non_strings
+end
+
+def count_elements
+end
+
+def merge_data
+end
+
+def find_cool
+end
+
+def organize_schools
+end
