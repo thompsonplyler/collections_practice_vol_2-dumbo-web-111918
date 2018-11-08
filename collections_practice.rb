ace3bb084be1478d98fabdb82b5b40b60f8771ae
Thompson Plyler <nealok.g@gmail.com>
2018-11-08 04:06:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,11 +3,11 @@ require "pry"
 def begins_with_r(array)
   result = true
   split_array = []
+  binding.pry
     array.each do |x|
     split_array << x.split
       if split_array[0] != "r"
         result = false
-        binding.pry
         break
       else
         result = true
